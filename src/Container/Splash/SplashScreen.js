21f810727e2c2d73d9d5d1096607fa2437c5c8e1
Kent <iamcxa@gmail.com>
2021-08-27 00:56:57 +0800

    feat(splash-screen): update weo splash screen

--- src/Container/Splash/SplashScreen.js
+++ src/Container/Splash/SplashScreen.js
@@ -1,23 +1,141 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import PropTypes from 'prop-types';
+import { isString } from 'lodash';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { CircleActions, UserActions } from 'App/Stores';
 
-import styles from './SplashScreenStyle';
+import SplashView from './SplashView';
 
 class SplashScreen extends React.Component {
-  componentDidMount() {
-    __DEV__ && console.log('@Enter SplashScreen!');
+  static propTypes = {
+    updateUserStore: PropTypes.func.isRequired,
+    fetchPostSignUp: PropTypes.func.isRequired,
+    fetchGetStayCircles: PropTypes.func.isRequired,
+    cleanUser: PropTypes.func.isRequired,
+    apiToken: PropTypes.string,
+    fcmToken: PropTypes.string,
+    currentState: PropTypes.string,
+    sceneKey: PropTypes.string.isRequired,
+    // hasGeolocationPermission: PropTypes.bool.isRequired,
+  };
+
+  static defaultProps = {
+    apiToken: '',
+    currentState: '',
+  };
+
+  timer = null;
+
+  async componentDidMount() {
+    // await this.checkLocationPermission();
+    const { apiToken, fcmToken } = this.props;
+
+    if (!apiToken && !fcmToken) {
+    }
   }
 
+  // shouldComponentUpdate(nextProps) {}
+
+  // async componentDidUpdate(prevProps, prevState) {
+  //   const { currentState } = this.props;
+  //   if (currentState !== prevProps.currentState && currentState === 'active') {
+  //     await this.checkLocationPermission();
+  //   }
+  // }
+
+  // checkLocationPermission = async () => {
+  //   let havePermission = await Permission.checkAndRequestPermission(
+  //     Permission.location,
+  //     false,
+  //   );
+  //   if (!havePermission) {
+  //     havePermission = await Permission.checkAndRequestPermission(
+  //       Permission.location,
+  //       true,
+  //     );
+  //   }
+  //   console.log('havePermission=>', havePermission);
+  //   if (havePermission === true) {
+  //     const { fetchGetStayCircles, sceneKey } = this.props;
+  //     fetchGetStayCircles(null, sceneKey);
+  //     if (this.timer) {
+  //       clearTimeout(this.timer);
+  //     }
+  //     this.timer = setTimeout(this.jumpToNextScreen, Config.SPLASH_REDIRECT_DELAY);
+  //   } else {
+  //     requestAnimationFrame(Dialog.requestLocationPermissionFromSystemAlert);
+  //   }
+  // };
+
+  jumpToNextScreen = () => {
+    const { apiToken, cleanUser } = this.props;
+    if (!apiToken || !isString(apiToken)) {
+      // skip intro since new version changes
+      // Actions.intro({ type: 'replace' });
+      // await this.autoLoginWithRandomUserData();
+      cleanUser();
+      // requestAnimationFrame(Actions.SignUpScreen);
+      this.autoLoginWithRandomUserData();
+    } else {
+      // requestAnimationFrame(Actions.HomeScreen);
+    }
+  };
+
+  // autoLoginWithRandomUserData = () => {
+  //   console.log('autoLoginWithRandomUserData');
+  //   // console.log('data', data);
+  //   const { fetchPostSignUp, fcmToken } = this.props;
+
+  //   const handleSignup = (token = false) => {
+  //     console.log('handleSignup token=>', token);
+  //     const avatarId = User.getRandomSeed(12);
+  //     const nickName = User.getRandomNickName();
+  //     const data = {
+  //       avatarKey: `Avatar${avatarId}`,
+  //       nickname: nickName,
+  //       identifier: `${new Date().getTime()}${Math.floor(Math.random() * 100 + 1)}`,
+  //       deviceInfo: {
+  //         deviceToken: token || this.props.fcmToken,
+  //       },
+  //     };
+  //     fetchPostSignUp(data);
+  //   };
+  //   if (!fcmToken) {
+  //     Dialog.requestFcmPermissionAlert({
+  //       onSuccess: handleSignup,
+  //       onFailure: handleSignup,
+  //       onCancel: handleSignup,
+  //     });
+  //   } else {
+  //     handleSignup();
+  //   }
+  // };
+
   render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.logo}>
-          {/* You will probably want to insert your logo here */}
-          <Text>LOGO</Text>
-        </View>
-      </View>
-    );
+    return <SplashView />;
   }
 }
 
-export default SplashScreen;
+export default connect(
+  (state, props) => ({
+    sceneKey: props.name,
+    apiToken: state.user.apiToken,
+    fcmToken: state.user.fcmToken,
+    currentState: state.appState.currentState,
+    // hasGeolocationPermission: AppPermissionSelectors.hasThisPermission(
+    // Permission.GEOLOCATION_LOW,
+    // )(state),
+  }),
+  (dispatch) =>
+    bindActionCreators(
+      {
+        fetchGetStayCircles: CircleActions.fetchGetStayCircles,
+        updateUserStore: UserActions.updateUserStore,
+        fetchPostSignUp: UserActions.fetchPostSignUp,
+        cleanUser: UserActions.cleanUser,
+        fetchPostAutoSignUp: UserActions.fetchPostAutoSignUp,
+      },
+      dispatch,
+    ),
+)(SplashScreen);
--- src/Container/Splash/SplashScreenStyle.js
+++ src/Container/Splash/SplashScreenStyle.js
@@ -1,20 +1,21 @@
 import { StyleSheet } from 'react-native';
-import { Colors } from '~/Theme';
+import Colors from 'App/Theme/Colors';
+import Styles from 'App/Theme/Styles';
 
 export default StyleSheet.create({
   container: {
-    // ...Styles.screen.container,
+    ...Styles.screen.container,
+    display: 'flex',
+    justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: Colors.primary,
-    display: 'flex',
-    justifyContent: 'center',
   },
   logo: {
-    alignItems: 'center',
-    backgroundColor: Colors.white,
     display: 'flex',
-    height: 70,
     justifyContent: 'center',
+    alignItems: 'center',
+    height: 70,
     width: 70,
+    backgroundColor: 'white',
   },
 });
